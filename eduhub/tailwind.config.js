ad00542ad998c4796bb0f6bfcc40c58c8ecc2b66
shao <68085490+shaoming11@users.noreply.github.com>
2025-04-07 22:24:43 -0400

    home page

--- eduhub/tailwind.config.js
+++ eduhub/tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          midnight: '#4682B4',
+          bluegray: '#5885af',
+          darkblue: '#274472',
+          babyblue: '#c3e0e5',
+          azure: '#6495ED'
+        },
+      },
     },
     plugins: [],
 };
